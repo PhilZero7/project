6e1939454be4bb02e224368895bc7a5813a61953
vsunks <vsunks@163.com>
2022-06-08 15:52:43 +0800

    logout

--- src/main/java/com/itheima/reggie/web/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/web/controller/EmployeeController.java
@@ -36,52 +36,69 @@ public class EmployeeController {
     @Autowired
     HttpServletRequest request;
 
-/**
- * 登录，验证用户名、密码、用户是否禁用。
- * @param loginDto 包含用户名和密码的实体
- * @return 是否登录成功，及对应的提示
- */
-@PostMapping("/login")
-public R login(@RequestBody LoginDto loginDto) {
-    String password = loginDto.getPassword();
-    log.info("开始登录。用户名：{}；密码：{}", loginDto.getUsername(), password);
+    /**
+     * 登录，验证用户名、密码、用户是否禁用。
+     *
+     * @param loginDto 包含用户名和密码的实体
+     * @return 是否登录成功，及对应的提示
+     */
+    @PostMapping("/login")
+    public R login(@RequestBody LoginDto loginDto) {
+        String password = loginDto.getPassword();
+        log.info("开始登录。用户名：{}；密码：{}", loginDto.getUsername(), password);
 
-    // StringUtils导commons的包，而非SpringFramwork的
-    if (StringUtils.isNotBlank(password)) {
-        //1. 加密提交的`password`，得到密文
-        String pwd = DigestUtils.md5DigestAsHex(password.getBytes());
+        // StringUtils导commons的包，而非SpringFramwork的
+        if (StringUtils.isNotBlank(password)) {
+            //1. 加密提交的`password`，得到密文
+            String pwd = DigestUtils.md5DigestAsHex(password.getBytes());
 
-        //2. 判断员工是否存在
-        // 2.1 根据用户名查询
-        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(loginDto.getUsername() != null, Employee::getUsername,
-                loginDto.getUsername());
-        Employee employee = employeeService.getOne(queryWrapper);
+            //2. 判断员工是否存在
+            // 2.1 根据用户名查询
+            LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(loginDto.getUsername() != null, Employee::getUsername,
+                    loginDto.getUsername());
+            Employee employee = employeeService.getOne(queryWrapper);
 
-        // 2.2 判断查询到的员工是否为空
-        if (employee == null) {
-            return R.fail("用户不存在");
+            // 2.2 判断查询到的员工是否为空
+            if (employee == null) {
+                return R.fail("用户不存在");
+            }
+
+            //3. 判断密码是否正确
+            if (!pwd.equals(employee.getPassword())) {
+                return R.fail("密码错误");
+            }
+
+            //4. 判断员工是否被禁用
+            if (0 == employee.getStatus()) {
+                return R.fail("都离职了，还来干嘛！");
+            }
+
+            //5. 登陆成功，把员工`ID`存入`session`
+            request.getSession().setAttribute("employee", employee.getId());
+
+            //6. 响应页面登陆成功。
+            // 登录成功，需要将员工对象响应到前台，否则前台右上角看不到当前登录的用户名。
+            return R.success("登录成功", employee);
         }
 
-        //3. 判断密码是否正确
-        if (!pwd.equals(employee.getPassword())) {
-            return R.fail("密码错误");
-        }
-
-        //4. 判断员工是否被禁用
-        if (0 == employee.getStatus()) {
-            return R.fail("都离职了，还来干嘛！");
-        }
-
-        //5. 登陆成功，把员工`ID`存入`session`
-        request.getSession().setAttribute("employee", employee.getId());
-
-        //6. 响应页面登陆成功
-        return R.success("登录成功",employee);
+        return R.fail("密码错误");
     }
 
-    return R.fail("密码错误");
-}
 
+    /**
+     * 登出注销
+     * @return 操作的结果
+     */
+    @PostMapping("/logout")
+    public R logout() {
+        log.info("登出注销");
+
+        // 清理session
+        request.getSession().removeAttribute("employee");
+
+        // 响应操作结果
+        return R.success("注销成功");
+    }
 
 }
